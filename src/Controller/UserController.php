0c69e448ba9bed74b0fe4066d231e501fe158a2f
Mustafa KARAKAŞ <m.karakas@media.local>
2023-11-19 15:04:29 +0300

    project revize

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -7,9 +7,29 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\User;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class UserController extends AbstractController
 {
+    #[Route('/login', name: 'app_login')]
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('user/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error,
+        ]);
+    }
+
+    #[Route('/logout', name: 'app_logout')]
+    public function logout(): void
+    {
+        // Çıkış işlemi burada işlenir
+    }
+
     #[Route('/register', name: 'app_register')]
     public function register(Request $request): Response
     {
@@ -21,8 +41,6 @@ class UserController extends AbstractController
 
                 $password = $request->request->get('password');
                 $user->setPassword($password);
-
-                $user->setRoles(['ROLE_USER']);
 
                 $entityManager = $this->getDoctrine()->getManager();
                 $entityManager->persist($user);
